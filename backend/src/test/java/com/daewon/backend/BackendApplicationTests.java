9630792b104437c9a488f08f7b9e1b9d9c7cb40a
김대원 <kdanny99@naver.com>
2026-04-15 11:16:18 +0900

    init: set up project

---
+++ backend/src/test/java/com/daewon/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daewon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
